3ebfcde6a683363e8539c07021b645881f7d2af4
Alan Wilson-Langman <alan@inspectobot.com>
2022-03-16 16:02:58 -0500

    init release

---
+++ simple_sfcw_radar.py
@@ -0,0 +1,118 @@
+import time
+
+import matplotlib.pyplot as plot
+import numpy as np
+
+from lib.signal_processing import LowPassFilter, IQDemodulator
+from lib.digital_backend import RedPitayaSampler
+from lib.rf_source import RFsource
+
+if __name__ == '__main__':
+    num_samples = 2048
+    if_filter = IQDemodulator(f_lo=36e6, fc=4e6, ft=2e6, number_of_taps=128, fs=122.88e6, t_sample=1e-6, n=num_samples)
+    bb_filter = LowPassFilter(fc=0.5e6, ft=1e6, number_of_taps=128, fs=122.88e6, ts=1e-6, N=num_samples)
+
+    synth = RFsource(start_frequency=2000,
+                     step_frequency=10,
+                     number_of_frequencies=101,
+                     intermediate_frequency=32,
+                     transmit_power=0,
+                     lo_power=15,
+                     port='/dev/cu.usbmodem206834A0z4E561')
+    synth.connect()
+    synth.set_frequency()
+    synth.set_power()
+    synth.enable()
+
+    rpi = RedPitayaSampler(n=num_samples)
+    rpi.connect()
+    rpi.configure_sampler()
+    rpi.start_sampler()
+    time.sleep(1)
+    rpi.trigger_sampler()
+
+    time.sleep(1)
+
+    dut = rpi.get_data(channel=1)
+    ref = rpi.get_data(channel=2)
+
+    #ref_n = ref / np.max(np.abs(ref))
+    ref_n = ref
+    dut_iq, lo, x_mixer_o = if_filter(dut)
+    ref_iq, lo, x_mixer_o = if_filter(ref_n)
+
+    bb_mixer = np.multiply(ref_iq, np.conj(dut_iq))
+    bb = bb_filter(bb_mixer)
+    bb = bb_filter(bb)
+
+    x = np.mean(bb[200:num_samples//2])
+    print(f"f={synth.get_frequency()[0]}, Mag(x) = {np.abs(x)}, I={np.real(x)},Q={np.imag(x)}")
+
+    t = rpi.get_t()
+    f = rpi.get_f()
+
+    plot.figure(1)
+    plot.plot(t[:100] / 1e-6, dut[:100])
+    plot.plot(t[:100] / 1e-6, ref[:100])
+    plot.xlabel('Time (us)')
+    plot.ylabel('Voltage')
+    plot.title("Raw dut/ref channels")
+
+    plot.figure(2)
+    plot.plot(t[200:num_samples//2] / 1e-6, np.real(dut_iq[200:num_samples//2]))
+    plot.plot(t[200:num_samples//2] / 1e-6, np.real(ref_iq[200:num_samples//2]))
+    plot.xlabel('Time (us)')
+    plot.ylabel('Voltage')
+    plot.title("Raw dut/ref IQ")
+
+    plot.figure(3)
+    z = np.fft.ifft(dut * np.kaiser(num_samples, 9))
+    plot.plot(f[:num_samples // 2] / 1e6, 20 * np.log10(abs(z[:num_samples // 2])))
+    plot.grid()
+    plot.title('Spectrum raw dut')
+
+    plot.figure(4)
+    z = np.fft.ifft(dut_iq * np.kaiser(num_samples, 9))
+    plot.plot(f[:num_samples // 2] / 1e6, 20 * np.log10(abs(z[:num_samples // 2])), alpha=0.5)
+    z = np.fft.ifft(ref_iq * np.kaiser(num_samples, 9))
+    plot.plot(f[:num_samples // 2] / 1e6, 20 * np.log10(abs(z[:num_samples // 2])), alpha=0.5)
+
+    plot.grid()
+    plot.title('Spectrum post IQ demod')
+
+    plot.figure(5)
+    z = np.fft.fft(np.conj(x_mixer_o) * np.kaiser(num_samples, 9))
+    plot.plot(f[:num_samples // 2] / 1e6, 20 * np.log10(abs(z[:num_samples // 2])))
+    plot.grid()
+    plot.title('Spectrum Pre filtering of IQ')
+
+    plot.figure(6)
+    z = np.fft.fft(np.conj(lo) * np.kaiser(num_samples, 9))
+    plot.plot(f[:num_samples // 2] / 1e6, 20 * np.log10(abs(z[:num_samples // 2])))
+    plot.grid()
+    plot.title('Spectrum LO')
+
+    plot.figure(7)
+    plot.plot(t[:500] / 1e-6, np.real(lo[:500]))
+    plot.xlabel('Time (us)')
+    plot.ylabel('Voltage')
+    plot.title("Local Oscillator")
+
+    plot.figure(8)
+    plot.plot(t[200:num_samples//2] / 1e-6, np.real(bb[200:num_samples//2]))
+    plot.plot(t[200:num_samples//2] / 1e-6, np.imag(bb[200:num_samples//2]))
+    plot.xlabel('Time (us)')
+    plot.ylabel('Voltage')
+    plot.title("Raw dut IQ")
+
+    plot.figure(9)
+    z = np.fft.ifft(bb * np.kaiser(num_samples, 9))
+    plot.plot(f[:num_samples // 2] / 1e6, 20 * np.log10(abs(z[:num_samples // 2])))
+    plot.grid()
+    plot.xlim([0, 10])
+    plot.title('Spectrum of base band')
+
+    plot.show()
+
+    rpi.stop_sampler()
+    synth.close()
---
+++ simple_sfcw_radar_sweep.py
@@ -0,0 +1,115 @@
+import time
+import sys
+from lib.signal_processing import BandPassFilter, LowPassFilter, IQDemodulator
+from lib.digital_backend import RedPitayaSampler
+from lib.rf_source import  RFsource
+import matplotlib.pyplot as plot
+import struct
+import numpy as np
+from scipy import signal
+
+
+if __name__ == '__main__':
+    num_samples = 2048
+    if_filter = IQDemodulator(f_lo=36e6, fc=4e6, ft=2e6, number_of_taps=128, fs=122.88e6, t_sample=1e-6, n=num_samples)
+    bb_filter = LowPassFilter(fc=0.5e6, ft=1e6, number_of_taps=128, fs=122.88e6, ts=1e-6, N=num_samples)
+
+    synth = RFsource(start_frequency=1000,
+                     step_frequency=10,
+                     number_of_frequencies=301,
+                     intermediate_frequency=32,
+                     transmit_power=0,
+                     lo_power=15,
+                     port='/dev/cu.usbmodem206834A04E561')
+    synth.connect()
+    synth.set_frequency()
+    synth.set_power()
+    synth.enable()
+    time.sleep(0.5)
+
+    N = synth.get_number_frequency_steps()
+    x = np.zeros(N, dtype=complex)
+
+    rpi = RedPitayaSampler(n=num_samples)
+    rpi.connect()
+    rpi.configure_sampler()
+
+    print("Scanning ", end='')
+    for i in range(N):
+        rpi.trigger_sampler()
+        time.sleep(0.05)
+        dut = rpi.get_data(channel=1)
+        ref = rpi.get_data(channel=2)
+        ref_n = ref/np.max(np.abs(ref))
+        ref_iq,lo,a=  if_filter (ref_n)
+        dut_iq,lo,a =  if_filter (dut)
+        bb_mixer = np.multiply(ref_iq,np.conj(dut_iq))
+        bb = bb_filter(bb_mixer)
+        bb= bb_filter(bb)
+        x[i] = np.mean(bb[200:len(bb)//2])
+        print(".", end='')
+        synth.frequency_step()
+        time.sleep(0.05)
+    print ("done")
+    f = synth.get_frequency_range()
+    plot.figure(1)
+    plot.plot(f, np.real(x))
+    plot.plot(f, np.imag(x))
+    plot.grid()
+    plot.xlabel('Frequency in MHz')
+    plot.ylabel('Amplitude in volts')
+
+    plot.figure(2)
+    plot.plot(f,np.angle(x))
+    plot.grid()
+    plot.xlabel('Frequency in MHz')
+    plot.ylabel('Phase in radians')
+
+    M=synth.get_number_frequency_steps()*32;
+    r_max = 3e8/2/(synth.get_frequency_step()*1e6)
+    r = np.linspace(0,r_max,M)
+
+    window = np.kaiser(N,9);
+    # window = signal.windows.dpss(N, 3)
+
+    z = np.fft.ifft(x*window,M)
+
+    plot.figure(3)
+    plot.plot(f,np.angle(x))
+    plot.grid()
+    plot.xlabel('Frequency in MHz')
+    plot.ylabel('Phase in radians')
+
+    plot.figure(4)
+    plot.plot(f, np.unwrap(np.angle(x)))
+    plot.grid()
+    plot.xlabel('Frequency in MHz')
+    plot.ylabel('Phase in radians')
+
+    plot.figure(5)
+    plot.plot(r, np.abs(z))
+    plot.grid()
+    plot.xlabel('Range in meters')
+    plot.ylabel('Amplitude in volts')
+    plot.title('lot of radar range profile (dB scale)')
+
+    plot.figure(6)
+    plot.plot(r, 20*np.log10(np.abs(z)))
+    plot.grid()
+    plot.xlabel('Range in meters')
+    plot.ylabel('Plot of power in dBW')
+    plot.title('Plot of radar range profile (dB scale)')
+
+    plot.figure(7)
+    plot.plot(r,np.real(z))
+    plot.plot(r,np.imag(z))
+    plot.grid()
+    plot.xlabel('Range in meters')
+    plot.ylabel('Plot of power in dBW')
+    plot.title('Plot of radar range profile (dB scale)')
+    plot.xlim([5,7])
+    plot.show()
+
+
+    rpi.stop_sampler()
+    synth.close()
