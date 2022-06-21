e823b6f4ec7cf1cd7fbff259184add55b51d7fe1
ian <ian@coinroutes.com>
2022-06-21 13:06:18 -0400

    added fast no-redis job system

---
+++ process_scan/process_simple_sfcw_radar_scan.py
@@ -0,0 +1,82 @@
+import sys
+sys.path.append("..")
+
+import time
+import sys
+import argparse
+import ctypes as c
+
+from radar.signal_processing import BandPassFilter, LowPassFilter, IQDemodulator
+from radar.client import CreateRadarProfile
+
+import matplotlib.pyplot as plot
+import numpy as np
+from scipy import signal
+
+import socket
+import h5py
+
+
+import multiprocessing
+
+
+import asyncio
+
+
+from scan_worker_job import process_sweep
+from concurrent.futures import ProcessPoolExecutor
+
+
+async def main(args):
+    executor = ProcessPoolExecutor(max_workers=4)
+    scanFile = h5py.File(args.data_file_path, "r")
+
+    sweepDataSets = scanFile.keys()
+
+    futures = []
+
+    loop = asyncio.get_event_loop()
+
+    for key in sweepDataSets:
+        print("Queuing key: " + key)
+        sweepDataSet = scanFile[key]
+
+        num_samples = sweepDataSet.attrs['sampleCount']
+        number_of_frequencies = sweepDataSet.attrs['frequencyCount']
+        number_of_channels = sweepDataSet.attrs['channelCount']
+        start_frequency = sweepDataSet.attrs['startFrequency']
+        step_frequency = sweepDataSet.attrs['stepFrequency']
+        intermediate_frequency = sweepDataSet.attrs['intermediateFreq']
+        transmit_power = sweepDataSet.attrs['transmitPower']
+        lo_power = sweepDataSet.attrs['loPower']
+        posePos = sweepDataSet.attrs['pose.pos']
+        poseRot = sweepDataSet.attrs['pose.rot']
+
+        RadarProfile = CreateRadarProfile(number_of_channels, num_samples, number_of_frequencies)
+        profile = RadarProfile()
+
+        profile.setArrayFromDataset(sweepDataSet)
+        #profile.saveCacheKey(r, args.data_file_path, key)
+
+        future = loop.run_in_executor(executor, process_sweep, None,
+            args.data_file_path, key, num_samples, number_of_frequencies,
+            number_of_channels, start_frequency, step_frequency,
+            intermediate_frequency, transmit_power, lo_power, posePos, poseRot,
+            profile.asArray())
+
+        futures.append(future)
+
+    scanFile.close()
+
+    print(await asyncio.gather(*futures))
+    print("done!!!!!")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process saved raw scan.')
+    parser.add_argument('-d', '--data-file-path', type=str, help="Path to saved raw data scan hdf5 file")
+    parser.add_argument('-j', '--number-of-processes', type=int, default=multiprocessing.cpu_count(), help="Number of worker processes to spawn")
+
+    args = parser.parse_args()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(args))
---
+++ process_scan/scan_worker_job.py
@@ -0,0 +1,91 @@
+import sys
+sys.path.append("..")
+
+from radar.signal_processing import BandPassFilter, LowPassFilter, IQDemodulator
+from radar.client import CreateRadarProfile
+
+import numpy as np
+from scipy import signal
+
+import h5py
+
+import asyncio
+
+import pprofile
+
+
+def process_sweep(ctx, filename, dataset_key,
+    num_samples,
+    number_of_frequencies,
+    number_of_channels,
+    start_frequency,
+    step_frequency,
+    intermediate_frequency,
+    transmit_power,
+    lo_power,
+    posePos,
+    poseRot, ProfileArray):
+    profiler = pprofile.Profile()
+    with profiler:
+        print("processing job", filename, dataset_key)
+
+        RadarProfile = CreateRadarProfile(number_of_channels, num_samples, number_of_frequencies)
+        profile = RadarProfile()
+
+        profile._array = ProfileArray # this is ratchet but whatever
+
+        if_filter = IQDemodulator(f_lo=36e6, fc=4e6, ft=1e6, number_of_taps=256, fs=122.88e6, t_sample=1e-6, n=num_samples)
+        bb_filter = LowPassFilter(fc=0.5e6, ft=1e6, number_of_taps=256, fs=122.88e6, ts=1e-6, N=num_samples)
+
+        N = number_of_frequencies
+        x = np.zeros(N, dtype=complex)
+        f = np.linspace(start_frequency, start_frequency + ((number_of_frequencies - 1) * step_frequency), number_of_frequencies)
+
+        for i in range(N):
+          dut = profile.getSamplesAtIndex(1, i)
+          ref = profile.getSamplesAtIndex(0, i)
+
+          ref_n = ref/np.max(np.abs(ref))
+          ref_iq,lo,a=  if_filter (ref_n)
+          dut_iq,lo,a =  if_filter (dut)
+          bb_mixer = np.multiply(ref_iq/np.abs(ref_iq),np.conj(dut_iq))
+          bb = bb_filter(bb_mixer)
+          bb = bb_filter(bb)
+          x[i] = np.mean(bb[200:len(bb)//2])
+
+        f = f[1:]
+        x = x[1:]
+        N=N-1
+
+        M=number_of_frequencies*10;
+        r_max = 3e8/2/(step_frequency*1e6)
+        r = np.linspace(0,r_max,M)
+
+        window = np.kaiser(N,9);
+        #window = signal.windows.dpss(N, 3)
+        #window = np.hamming(N)
+        x_w = x*window
+        x_w = x_w/np.max(np.abs(x_w))
+        z = np.fft.ifft(x_w,M)
+        z = z/np.max(np.abs(z))
+
+        scanFile = h5py.File(filename + '_' + dataset_key + "-range.hdf5", "w")
+
+        rangeProfileDataSet = scanFile.create_dataset('range-' + dataset_key, (z.size), dtype=np.clongdouble)
+
+        rangeProfileDataSet.attrs['sampleCount'] = num_samples
+        rangeProfileDataSet.attrs['frequencyCount'] = number_of_frequencies
+        rangeProfileDataSet.attrs['channelCount'] = number_of_channels
+        rangeProfileDataSet.attrs['start_frequency'] = start_frequency
+        rangeProfileDataSet.attrs['step_frequency'] = step_frequency
+        rangeProfileDataSet.attrs['intermediate_frequency'] = intermediate_frequency
+        rangeProfileDataSet.attrs['transmitPower'] = transmit_power
+        rangeProfileDataSet.attrs['loPower'] = lo_power
+        rangeProfileDataSet.attrs['pose.pos'] = posePos
+        rangeProfileDataSet.attrs['pose.rot'] = poseRot
+
+        rangeProfileDataSet.write_direct(z)
+
+        scanFile.close()
+    profiler.dump_stats(filename + '_' + dataset_key + '.txt')
+    print("Job Done {}".format(dataset_key))
