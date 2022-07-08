17889d2ee51455e7a9d73d2a240f3c0eb0dbb0a0
brad <bradley@inspectobot@gmail.com{>
2022-07-08 15:57:23 -0500

    added functions to sync single or all folder(s)

--- s3sync.py
+++ s3sync.py
@@ -1,8 +1,46 @@
 import subprocess
-result = subprocess.run(['aws', 's3', 'sync', '/media/brad/DATA/output/2022-07-06T02:36:57.084556', 's3://gpr-scan-data/testFolder3', '--dryrun'], stdout=subprocess.PIPE)
-result = result.stdout.decode('utf-8')
+import os
 
-if(result == ""):
-    print('no changes')
-else:
-    print(result)
+#arguments = ['aws', 's3', 'sync', '/media/brad/DATA/output/2022-07-06T02:36:57.084556', 's3://gpr-scan-data/testFolder3', '--dryrun']
+#result = subprocess.run(arguments, stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+#if(result == ""):
+#    print('no changes')
+#else:
+#    print(result)
+
+outputList = os.listdir('/home/brad/Desktop/DATA/output')
+for folder in outputList:
+    path = os.path.join(os.path.expanduser('~'), 'Desktop', 'DATA', 'Output',folder)
+
+def syncCheck(result):
+    if(result == ""):
+            print('no changes')
+    else:
+        print(result)
+
+outputList = os.listdir('/home/brad/Desktop/DATA/output')
+folderC = 0
+
+def syncAll():
+    for folder in outputList:
+    #print(folder)
+        path = os.path.join(os.path.expanduser('~'), 'Desktop', 'DATA', 'output',folder)
+        #if folderC < 1:
+        arguments = ['aws', 's3', 'sync', path, ('s3://gpr-scan-data/' + folder), '--dryrun']
+        result = subprocess.run(arguments, stdout=subprocess.PIPE).stdout.decode('utf-8')
+        syncCheck(result)
+
+        folderC += 1
+
+def sync(folder):
+    path = os.path.join(os.path.expanduser('~'), 'Desktop', 'DATA', 'output',folder)
+    arguments = ['aws', 's3', 'sync', path, ('s3://gpr-scan-data/' + folder), '--dryrun']
+    arguments = ['aws', 's3', 'sync', path, ('s3://gpr-scan-data/' + folder), '--dryrun']
+    result = subprocess.run(arguments, stdout=subprocess.PIPE).stdout.decode('utf-8')
+    syncCheck(result)
+
+sync('2022-07-06T02:36:57.084556')
+
+
+print(folderC)
