55c1a6ec2aef09a87cc35bfea85cfd59b4e29c4d
brad <bradley@inspectobot@gmail.com{>
2022-07-11 13:28:18 -0500

    converted all file functionality to use json formatting

--- s3sync.py
+++ s3sync.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import json
 
 #arguments = ['aws', 's3', 'sync', '/media/brad/DATA/output/2022-07-06T02:36:57.084556', 's3://gpr-scan-data/testFolder3', '--dryrun']
 #result = subprocess.run(arguments, stdout=subprocess.PIPE).stdout.decode('utf-8')
@@ -17,22 +18,30 @@ def syncCheck(result):
     else:
         print(result)
 
-def fileWrite(input): #copy over text from input to jsonTest.txt file
-    file = open("jsonTest.txt", "r+")
-    for l in input:
-        file.write(l)
-    file.close()
 
-def fileRead(): #read content of jsonTest.txt by line
-    file = open("jsonTest.txt", "r")
-    for l in file:
-        print(l)
-    if(os.stat("jsonTest.txt").st_size == 0): #check if file is empty, useful for fileWipe()
-        print("File is empty")
-    file.close()
+def fileWrite(name, input):
+    if(os.stat("jsonTest.json").st_size == 0): #if json file is empty, create new dictionary
+        fOut = {name: input}
+    else: #if json file has contents: 
+        fOut = fileRead() #read existing dictionary from json file
+        fOut.update({name: input}) #add new key and contents to file
+    with open("jsonTest.json", "r+") as file:
+        json.dump(fOut, file)
+
+def fileRead(): #returns contents of the json file as a dictionary
+    with open('jsonTest.json', 'r') as file:
+        jsonObj = json.load(file)
+    return jsonObj
+
+def filePrint():
+    with open('jsonTest.json', 'r') as file:
+        jsonObj = json.load(file)
+
+    for key in jsonObj:
+        print(jsonObj[key])
 
 def fileWipe(): #Delete content of jsonTest.txt file
-    file = open("jsonTest.txt", "r+")
+    file = open("jsonTest.json", "r+")
     file.truncate()
 
 
@@ -50,16 +59,28 @@ def syncAll():
         syncCheck(result) #if we have results, print them
 
         folderC += 1
+        fileWrite(folder, result)
     print('There are ' + str(folderC) + ' files in this directory')
 
+#result = ""
 def sync(folder):
+    global result
     path = os.path.join(os.path.expanduser('~'), 'Desktop', 'DATA', 'output',folder)
     arguments = ['aws', 's3', 'sync', path, ('s3://gpr-scan-data/' + folder), '--dryrun']
     result = subprocess.run(arguments, stdout=subprocess.PIPE).stdout.decode('utf-8')
     syncCheck(result)
-    fileWrite(result)
 
-sync('2022-07-06T02:52:41.005402')
+    #fileWipe()
 
+#sync('2022-07-06T02:52:41.005402')
+#print(outputList)
+##fileWipe()
+#fileWrite('2022-07-06T02:52:41.005402', result)
+#sync('2022-07-06T05:59:35.153863')
+#fileWrite('2022-07-06T05:59:35.153863', result)
+#filePrint()
+##syncAll()
+
+filePrint()
 
 
